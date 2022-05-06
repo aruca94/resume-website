c9d2d8796d26ca5e8bb0510e0f24c243ec205bd8
Josh <aftershock840@gmail.com>
2022-05-06 03:52:35 -0400

    added 2 of 3 project images with javascript

--- js/projects.js
+++ js/projects.js
@@ -3,47 +3,48 @@
 
 //  TODO: Add image clips of projects to each project
 //  TODO: Add classes as needed
-
-
-
-
-let firstProjectHeading = document.getElementById('project1');
-let firstProjectHeader = document.createElement('h2');
-let firstProjectHeaderText = document.createTextNode('Project 1');
+    //project 1 header
+const firstProjectHeading = document.getElementById('project1');
+const firstProjectHeader = document.createElement('h2');
+const firstProjectHeaderText = document.createTextNode('Project 1');
 firstProjectHeader.appendChild(firstProjectHeaderText);
 firstProjectHeading.appendChild(firstProjectHeader);
-
-let firstProjectParagraphTag = document.createElement('p');
-let firstProjectParagraphText = document.createTextNode('Project 1 paragraph here');
+    //project 1 paragraph
+const firstProjectParagraphTag = document.createElement('p');
+const firstProjectParagraphText = document.createTextNode('Project 1 paragraph here');
 firstProjectParagraphTag.appendChild(firstProjectParagraphText);
 firstProjectHeading.appendChild(firstProjectParagraphTag);
-
+    //project 1 image
+const imgOne = document.createElement('img');
+imgOne.src= "images/tea-cozy.PNG";
+firstProjectHeading.appendChild(imgOne);
 // project 2 dom manipulation start
 
-//h2 and text for h2 below
-let secondProjectHeading = document.getElementById('project2');
-let secondProjectHeader = document.createElement('h2');
-let secondProjectHeaderText = document.createTextNode('Project 2');
+    //project 2 header
+const secondProjectHeading = document.getElementById('project2');
+const secondProjectHeader = document.createElement('h2');
+const secondProjectHeaderText = document.createTextNode('Project 2');
 secondProjectHeader.appendChild(secondProjectHeaderText);
 secondProjectHeading.appendChild(secondProjectHeader);
-// paragraph for h2 below
-let secondProjectParagraphTag = document.createElement('p');
-let secondProjectParagraphText = document.createTextNode('Project 2 paragraph here');
+    //project 2 paragraph
+const secondProjectParagraphTag = document.createElement('p');
+const secondProjectParagraphText = document.createTextNode('Project 2 paragraph here');
 secondProjectParagraphTag.appendChild(secondProjectParagraphText);
 secondProjectHeading.appendChild(secondProjectParagraphTag);
-//create p tag
-//create text for p tag
-//append text to p tag
-//append p tag to project2 id in html
-
+    //project 2 image
+const imgTwo = document.createElement('img');
+imgTwo.src = "images/dom-manipulation-landing-page.PNG";
+secondProjectHeading.appendChild(imgTwo);
 // project 3 dom manipulation start
-let thirdProjectHeading = document.getElementById('project3');
-let thirdProjectHeader = document.createElement('h2');
-let thirdProjectHeaderText = document.createTextNode('Project 3');
+    //project 3 header
+const thirdProjectHeading = document.getElementById('project3');
+const thirdProjectHeader = document.createElement('h2');
+const thirdProjectHeaderText = document.createTextNode('Project 3');
 thirdProjectHeader.appendChild(thirdProjectHeaderText);
 thirdProjectHeading.appendChild(thirdProjectHeader);
-// paragraph for h2 below
-let thirdProjectParagraphTag = document.createElement('p');
-let thirdProjectParagraphText = document.createTextNode('Project 3 paragraph here');
+    //project 3 paragraph
+const thirdProjectParagraphTag = document.createElement('p');
+const thirdProjectParagraphText = document.createTextNode('Project 3 paragraph here');
 thirdProjectParagraphTag.appendChild(thirdProjectParagraphText);
 thirdProjectHeading.appendChild(thirdProjectParagraphTag);
+    //project 3 image
